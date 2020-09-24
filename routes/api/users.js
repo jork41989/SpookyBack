78ad11698dcf5ee0808e0cf52ef34325375e4227
jork41989 <jork41989@gmail.com>
2020-09-23 23:38:02 -0400

    attempting to start backend

---
+++ routes/api/users.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+const validateUserInput = require('../../validation/login');
+
+router.post('/login', (req, res) => {
+
+  const {
+    errors,
+    isValid
+  } = validateUserInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  // Check to make sure nobody has already registered with a duplicate email
+  User.findOne({
+      email: req.body.email
+    })
+    .then(user => {
+      if (user) {
+        // returns user
+                jwt.sign(
+                  payload,
+                  keys.secretOrKey,
+                  // Tell the key to expire in one hour
+                  {
+                    expiresIn: 3600
+                  },
+                  (err, token) => {
+                    res.json({
+                      success: true,
+                      token: 'Bearer ' + token,
+                      username: user.username,
+                      email: user.email,
+                      admin: user.admin
+                    });
+                  });
+        } else {
+        // Otherwise create a new user
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          admin: false
+        })
+        newUser.save()
+          .then(user => res.json(user))
+
+      }
+    })
+})
---
+++ validation/login.js
@@ -0,0 +1,22 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateUserInput(data) {
+  let errors = {};
+
+  data.email = validText(data.email) ? data.email : '';
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
